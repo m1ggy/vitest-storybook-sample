7007963c0f6ebf422371ea1e8e7acabcdd03c4f2
m1ggy <miguel.buising1@gmail.com>
2023-07-14 11:35:05 +0800

    changes: - remove vitest substitute with @storybook/jest

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,17 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        alias: {
-            /**
-             * Storybook (specifically the interactions addon) requires that we use their
-             *   instrumented version of jest-expect. So our storybook does so. To make
-             *   these interactions still work in vitest we have @storybook/jest aliased
-             *   to resolve to vitest which, critically, exports { expect } as well.
-             */
-            "@storybook/jest": "vitest",
-        },
-    },
     test: {
         globals: true,
         environment: "jsdom",
